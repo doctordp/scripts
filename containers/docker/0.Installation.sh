c0860ebc5c168d02b548b7e34292ebcd15ed6c46
dominicdemeterca <dominicdemeterca@gmail.com>
2022-09-27 13:39:30 +0200

    update docker installation

--- containers/docker/0.Installation.sh
+++ containers/docker/0.Installation.sh
@@ -19,3 +19,15 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin --yes
 
+sudo systemctl enable docker.service
+
+sudo iptables -A INPUT -p tcp --dport 2377 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 7946 -j ACCEPT
+sudo iptables -A INPUT -p udp --dport 7946 -j ACCEPT
+sudo iptables -A INPUT -p udp --dport 4789 -j ACCEPT
+sudo iptables -A INPUT -p esp -j ACCEPT
+sudo iptables -A OUTPUT -p esp -j ACCEPT
+
+cp /usr/lib/systemd/network/99-default.link /etc/systemd/network
+
+echo "TransmitChecksumOffload=false" >> /etc/systemd/network/99-default.link
